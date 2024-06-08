a330d4a4a2f63655ba62c3175422fb90387d56da
simplesec <97812410+simpletechgithub@users.noreply.github.com>
2024-06-08 19:21:36 +0100

    Update dcandforestsetup.ps1

--- testlab/dcs/dcandforestsetup.ps1
+++ testlab/dcs/dcandforestsetup.ps1
@@ -1,11 +1,11 @@
-#Create 'basicvlab' with powershell
+#Create 'domain' with powershell
 #Run on your new Domain Controller. This script has been tested successfully in the author's lab environment.
-#working  12/2023 
+#working  Jun/2024 
 
 $NewIPAddress = "10.0.0.1" #Define IP address
-Rename-NetAdapter -Name “Ethernet0" -NewName “testlab" #Rename the network adapter
-New-NetIPAddress $NewIPAddress -InterfaceAlias "testlab" -PrefixLength 24 #Set IP address
-Set-DnsClientServerAddress -InterfaceAlias "testlab" -ServerAddresses 127.0.0.1 #Set DNS 
+Rename-NetAdapter -Name “Ethernet0" -NewName “domain" #Rename the network adapter
+New-NetIPAddress $NewIPAddress -InterfaceAlias "domain" -PrefixLength 24 #Set IP address
+Set-DnsClientServerAddress -InterfaceAlias "domain" -ServerAddresses 127.0.0.1 #Set DNS 
 Disable-NetAdapterBinding -Name "*" -ComponentID ms_tcpip6 #turn off IPv6
 ipconfig
 Set-NetFirewallProfile -Enabled False #disable firewall (use with caution)
@@ -16,17 +16,17 @@ Install-WindowsFeature DHCP -IncludeManagementTools #Install DHCP
   netsh dhcp add securitygroups #This adds DHCP Administrators and DHCP Users
   Restart-Service dhcpserver
 
-#Install AD Forest "basicvlab.local"
+#Install AD Forest "domain.local"
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
-Install-ADDSForest -DomainName basicvlab.local
+Install-ADDSForest -DomainName domain.local
 
 Import-Module ADDSDeployment
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath "C:\Windows\NTDS" `
 -DomainMode "WinThreshold" `
--DomainName "basicvlab.local" `
--DomainNetbiosName "basicvlab" `
+-DomainName "domain.local" `
+-DomainNetbiosName "domain" `
 -ForestMode "WinThreshold" `
 -InstallDns:$true `
 -LogPath "C:\Windows\NTDS" `
@@ -35,6 +35,6 @@ Install-ADDSForest `
 -Force:$true
 
 #Check that admin password is not stored in the XML files (Sysvol mining exploit)
-#\\basicvlab\SYSVOL\basicvlab\Policies\
+#\\domain\SYSVOL\domain\Policies\
 
-#findstr /S /I cpassword \\basicvlab.local\sysvol\basicvlab.local\policies\*.xml
+#findstr /S /I cpassword \\domain.local\sysvol\domain.local\policies\*.xml
