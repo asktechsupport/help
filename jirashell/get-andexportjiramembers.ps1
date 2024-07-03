5466144d6fa36c96fbdbdaeb3f8b0f6a36b5652d
simplesec <97812410+simpletechgithub@users.noreply.github.com>
2024-07-03 19:59:29 +0100

    not working.

--- jirashell/get-andexportjiramembers.ps1
+++ jirashell/get-andexportjiramembers.ps1
@@ -1,4 +1,4 @@
-  Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
  # Ensure the ImportExcel module is installed
 if (-Not (Get-Module -ListAvailable -Name ImportExcel)) {
    Install-Module -Name ImportExcel -Scope CurrentUser -Force
@@ -7,7 +7,6 @@ if (-Not (Get-Module -ListAvailable -Name ImportExcel)) {
 # Define the vars you'll need
     $site = "https://asktechsupport.atlassian.net"
     $email = "help@asktechsupport.co.uk"
-    $apiKey = "" #!!!-ENTER IN YOUR API KEY-!!!
     $plainText = "${email}:${apiKey}" # Combine email and API key with a colon
     $base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes($plainText)) # Encode the combined string in base64
     # Set up headers
@@ -16,28 +15,26 @@ if (-Not (Get-Module -ListAvailable -Name ImportExcel)) {
        Accept = "application/json"
        ContentType = "application/json"
     }
-    $groupnames = "$site/rest/api/3/groups/picker?expand=groups"
+    $groupnames = "$site/rest/api/3/groups/picker?expand=groups" | select name
     # Define the Jira API endpoint
-    $getgroupmembersapi = "$site/rest/api/3/group/member?maxResults=250&groupname=group1&includeInactiveUsers=false" | select emailAddress
+    $getgroupmembersapi = "$site/rest/api/3/group/member?maxResults=250&groupname=group1&includeInactiveUsers=false" | select values | foreach { $_.values} | select -ExpandProperty emailAddress
     $getgroupmembers = $getgroupmembersapi
 # Define the location of the spreadsheet
 $jiragroupsxlsx = "C:\Users\$env:USERNAME\Downloads\temp\Jira Groups.xlsx"
 $openspreadsheet = $jiragroupsxlsx
  
 # Make the API request
-#$callgetgroupmembers = Invoke-RestMethod -Uri $getgroupmembersapi -Method Get -Headers $headers | select values | foreach { $_.values} | select -ExpandProperty emailAddress
+$callgetgroupmembers = Invoke-RestMethod -Uri $getgroupmembersapi -Method Get -Headers $headers | select values | foreach { $_.values} | select -ExpandProperty emailAddress
  
 # Check if the Excel file exists; create it if it does not
         if (-Not (Test-Path $jiragroupsxlsx)) {
            New-ExcelWorkbook -Path $jiragroupsxlsx
             }
  
-$members = $getgroupmembers | foreach-object {
+$members = $callgetgroupmembers | foreach-object {
    [pscustomobject]@{GroupName = "$groupnames"; EmailAddress = $_}
 }
-#$apollocontributorsmembers = $apollocontributorsmembersemails | foreach-object {
-   #[pscustomobject]@{GroupName = "apollocontributorsmembers"; EmailAddress = $_}
-#} 
+
 $members | Export-Excel -Path $jiragroupsxlsx
  
  
