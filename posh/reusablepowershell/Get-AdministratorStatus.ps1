8fa45e6a40043ef0aef26726b15e3db03fbd84f0
help@asktechsupport <help@asktechsupport.co.uk>
2024-08-24 11:18:17 +0100

    Update Get-AdministratorStatus.ps1
    source

--- posh/reusablepowershell/Get-AdministratorStatus.ps1
+++ posh/reusablepowershell/Get-AdministratorStatus.ps1
@@ -1,6 +1,6 @@
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 # Function to check if running as administrator
-##   src: https://github.com/asktechsupport/help/tree/main/posh/reusablecode
+##   src: https://github.com/asktechsupport/help/tree/main/posh/reusablepowershell
 #------------------------------------------------------------------------------------------------------------------------------------------------#
 Unblock-File -Path "C:\Windows\System32\WindowsPowerShell\v1.0\Microsoft.PowerShellISE_profile.ps1"
 function Test-IsAdmin {
