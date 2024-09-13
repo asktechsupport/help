e62e94d1bdccecda5a302b59625c02d330af7074
help@asktechsupport <help@asktechsupport.co.uk>
2024-09-13 22:01:14 +0100

    Update stop-wsus-wizard.ps1

--- posh/reusablepowershell/stop-wsus-wizard.ps1
+++ posh/reusablepowershell/stop-wsus-wizard.ps1
@@ -1,5 +1,38 @@
-Install-WindowsFeature -Name UpdateServices, UpdateServices-Ui, UpdateServices-WidDB, UpdateServices-Services -IncludeManagementTools
+try {
+	# Variables
+	$WSUS_DIR = "C:/WSUS"
+	$LogFile = "C:/WSUSSetupLog.txt"
 
+	# Functions
+	
+	#to log messages
+  # https://aws.amazon.com/powershell/
+
+	function Log-Message {
+		param (
+			[string]$Message,
+			[string]$Level = "INFO"
+		)
+		$timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+		$logEntry = "$timestamp [$Level] $Message"
+		Add-Content -Path $LogFile -Value $logEntry
+		Write-Host $Message
+		# TODO 
+		# how to write host with colours based on error type
+	}
+Install-WindowsFeature -Name UpdateServices, UpdateServices-Ui, UpdateServices-WidDB, UpdateServices-Services -IncludeManagementTools
+# Post Install
+	Log-Message "Starting WSUS PostInstall"
+	try {
+		sl "C:\Program Files\Update Services\Tools\"
+		.\wsusutil.exe postinstall CONTENT_DIR=$WSUS_DIR > $PostInstall
+		Log-Message "WSUS PostInstall completed successfully. Output: $PostInstall" -Level "SUCCESS"
+		Log-Message "WSUS Version: $(Get-WsusServer).Version" -Level "INFO" #TODO fix this output
+	}
+	catch {
+		Log-Message "Failed to complete WSUS PostInstall: $_" -Level "ERROR"
+		exit
+	}
 	# SQL Patch
 	Log-Message "Patching SQL Minor version bug"
 	try {
