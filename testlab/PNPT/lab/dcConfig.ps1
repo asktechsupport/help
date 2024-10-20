1b106b3905c5dbf57c98fb5fe4d1135b154b55aa
help@asktechsupport <help@asktechsupport.co.uk>
2024-10-20 21:58:07 +0100

    create marvel.local domain controller
    Update dcConfig.ps1

--- testlab/PNPT/lab/dcConfig.ps1
+++ testlab/PNPT/lab/dcConfig.ps1
@@ -1,1 +1,35 @@
+#Create the PNPT lab with powershell
+#Run on your new Domain Controller. This script has been tested successfully in the author's lab environment.
+
+ 
+#Install DNS
+Install-WindowsFeature -Name DNS -IncludeManagementTools
+
+#Install DHCP
+Install-WindowsFeature DHCP -IncludeManagementTools
+  netsh dhcp add securitygroups #This adds DHCP Administrators and DHCP Users
+  Restart-Service dhcpserver
+
+#Install AD Forest "basicvlab.local"
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Install-ADDSForest -DomainName MARVEL.local
+
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainName "MARVEL.local" `
+-DomainNetbiosName "MARVEL" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true `
+-LogPath "C:\Windows\NTDS" `
+-NoRebootOnCompletion:$false `
+-SysvolPath "C:\Windows\SYSVOL" `
+-Force:$true
+
+#Check that admin password is not stored in the XML files (Sysvol mining exploit)
+#\\basicvlab\SYSVOL\basicvlab\Policies\
+
+#findstr /S /I cpassword \\basicvlab.local\sysvol\basicvlab.local\policies\*.xml
 
