c6f84859e17f27f51f557297aaf8dab97c8254e6
help@asktechsupport <help@asktechsupport.co.uk>
2025-06-26 22:51:30 +0100

    Update largest-100-files.ps1
    fixed error message

--- scripts/powershell/windows/files/largest-100-files.ps1
+++ scripts/powershell/windows/files/largest-100-files.ps1
@@ -4,16 +4,23 @@ foreach ($drive in Get-PSDrive -PSProvider FileSystem) {
     try {
         Write-Host "Scanning $($drive.Root)..."
         $files = Get-ChildItem -Path $drive.Root -Recurse -File -ErrorAction SilentlyContinue | ForEach-Object {
+            $size = $_.Length
+            $sizeAuto = if ($size -ge 1TB) {
+                '{0:N2} TB' -f ($size / 1TB)
+            } elseif ($size -ge 1GB) {
+                '{0:N2} GB' -f ($size / 1GB)
+            } elseif ($size -ge 1MB) {
+                '{0:N2} MB' -f ($size / 1MB)
+            } elseif ($size -ge 1KB) {
+                '{0:N2} KB' -f ($size / 1KB)
+            } else {
+                '{0:N2} B' -f $size
+            }
+
             [PSCustomObject]@{
                 'Path'         = $_.FullName
-                'Size (Auto)'  = Switch (
-                    { $_.Length -ge 1TB } { '{0:N2} TB' -f ($_.Length / 1TB); break }
-                    { $_.Length -ge 1GB } { '{0:N2} GB' -f ($_.Length / 1GB); break }
-                    { $_.Length -ge 1MB } { '{0:N2} MB' -f ($_.Length / 1MB); break }
-                    { $_.Length -ge 1KB } { '{0:N2} KB' -f ($_.Length / 1KB); break }
-                    Default             { '{0:N2} B'  -f ($_.Length) }
-                }
-                'Size (Bytes)' = $_.Length
+                'Size (Auto)'  = $sizeAuto
+                'Size (Bytes)' = $size
                 'Modified'     = $_.LastWriteTime
             }
         }
