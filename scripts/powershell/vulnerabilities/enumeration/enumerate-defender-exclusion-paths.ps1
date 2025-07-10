0a0e062435eaa84a4364ca2e4dbb4b09c1e183ee
help@asktechsupport <help@asktechsupport.co.uk>
2025-07-10 12:10:17 +0100

    Update enumerate-defender-exclusion-paths.ps1

--- scripts/powershell/vulnerabilities/enumeration/enumerate-defender-exclusion-paths.ps1
+++ scripts/powershell/vulnerabilities/enumeration/enumerate-defender-exclusion-paths.ps1
@@ -1,1 +1,6 @@
-C:\Program Files\Windows Defender\MpCmdRun.exe -Scan -ScanType 3 -File C:\|*
+#Powershell version
+& "C:\Program Files\Windows Defender\MpCmdRun.exe" -Scan -ScanType 3 -File "C:\"
+
+#cmd version
+"C:\Program Files\Windows Defender\MpCmdRun.exe" -Scan -ScanType 3 -File "C:\"
+
